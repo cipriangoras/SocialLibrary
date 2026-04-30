6748d8f8c98c1a17ca43cd2e5afcfea1415bdbc6
Ciprian Goras <ciprian.std@gmail.com>
2026-04-30 14:29:11 +0300

    fix

--- src/main/java/app/SocialLibraryAPI/entity/ReviewCommentEntity.java
+++ src/main/java/app/SocialLibraryAPI/entity/ReviewCommentEntity.java
@@ -17,9 +17,11 @@ public class ReviewCommentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
     private ReviewEntity review;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
